afcbfe81dd0050e922358932a46b6692fd1a69a1
Martin Nygren <m@rtin-nygren.se>
2017-01-08 15:21:43 +0000

    Initial Commit

---
+++ testRecursion/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testRecursion
+//
+//  Created by Martin Nygren on 08/01/2017.
+//  Copyright © 2017 Martin Nygren. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
