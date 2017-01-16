02a3f809db8d936fcab60a9833898196dc9cfe7b
Martin Nygren <m@rtin-nygren.se>
2017-01-16 22:35:46 +0000

    Compares wall clock time for recursive and imperative hermite polynomials.

--- testRecursion/main.swift
+++ testRecursion/main.swift
@@ -10,3 +10,32 @@ import Foundation
 
 print("Hello, World!")
 
+var sTimebaseInfo = mach_timebase_info()
+mach_timebase_info(&sTimebaseInfo)
+
+let numer = UInt64(sTimebaseInfo.numer)
+let denom = UInt64(sTimebaseInfo.denom)
+
+let tStartRecursive = mach_absolute_time()
+
+for var n in 3...30 {
+    let _ = recursiveHermite(N: n, x: 0.1 * Double(n))
+}
+
+let tEndRecursive = mach_absolute_time()
+
+let tStartImperative = mach_absolute_time()
+
+for var n in 3...30 {
+    let _ = imperativeHermite(N: n, x: 0.1 * Double(n))
+}
+
+let tEndImperative = mach_absolute_time()
+
+let elapsedRecursive = ((tEndRecursive-tStartRecursive) * numer) / denom
+
+let elapsedImperative = ((tEndImperative-tStartImperative) * numer) / denom
+
+print("Calculating Hermite polynomials recursively took \(elapsedRecursive)ns")
+
+print("Calculating Hermite polynomials imperatively took \(elapsedImperative)ns")
